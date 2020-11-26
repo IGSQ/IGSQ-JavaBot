fe6e4c919b4bbe566f445abb5c5e8819af5cd88c
TeaEyes <50794499+Inferno11@users.noreply.github.com>
2020-11-26 19:08:42 +0100

    Modhelp

--- src/org/igsq/igsqbot/commands/Modhelp_Command.java
+++ src/org/igsq/igsqbot/commands/Modhelp_Command.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import org.igsq.igsqbot.EmbedGenerator;
 import org.igsq.igsqbot.Yaml;
 
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
@@ -12,17 +14,19 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 public class Modhelp_Command {
 	private MessageChannel channel;
 	private User author;
+	private Member guildAuthor;
 
 	public Modhelp_Command(MessageReceivedEvent event) 
 	{
 		this.author = event.getAuthor();
 		this.channel = event.getChannel();
+		this.guildAuthor = event.getMember();
 		modHelpQuery();
 	}
 
 	private void modHelpQuery()
 	{
-		if(!author.isBot()) modhelp();
+		if(!author.isBot() && guildAuthor.hasPermission(Permission.MESSAGE_MANAGE)) modhelp();
 		else new EmbedGenerator(channel).text("You cannot Execute this command!\nThis may be due to sending it in the wrong channel or not having the required permission.").color(Color.RED).sendTemporary();
 	}
 
@@ -34,9 +38,9 @@ public class Modhelp_Command {
 		(
 				message ->
 				{
-					Yaml.updateField(message.getId() + ".help.user", "internal", author.getId());
-					Yaml.updateField(message.getId() + ".help.enabled", "internal", true);
-					Yaml.updateField(message.getId() + ".help.page", "internal", 1);
+					Yaml.updateField(message.getId() + ".modhelp.user", "internal", author.getId());
+					Yaml.updateField(message.getId() + ".modhelp.enabled", "internal", true);
+					Yaml.updateField(message.getId() + ".modhelp.page", "internal", 1);
 					message.addReaction("U+25C0").queue();	
 					message.addReaction("U+25B6").queue();		
 				}
