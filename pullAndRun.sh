64ce31b4a25296cb7127daf351aa38df5b460def
arynxd <aryn.folkard@null.net>
2021-01-18 20:53:24 +0000

    catch script exceptions

--- pullAndRun.sh
+++ pullAndRun.sh
@@ -2,5 +2,10 @@
 
 git pull
 gradle clean shadowJar
-clear
-java -jar build/libs/IGSQBot-0.0.1-all.jar
+buildStatus=$?
+
+if [[ buildStatus  -eq 0 ]]
+then
+  clear
+  java -jar build/libs/IGSQBot-0.0.1-all.jar
+fi
